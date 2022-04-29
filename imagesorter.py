17b281345683b1678e4176cc0b220020fdf6cd82
hk-beep <ga87vay@mytum.de>
2022-04-29 14:13:19 +0200

    changed imagesorter to sort into breeds

--- imagesorter.py
+++ imagesorter.py
@@ -6,17 +6,20 @@ infotext = 'annotations/trainval.txt'
 file1 = open(infotext, 'r')
 Lines = file1.readlines()
 split = len(Lines)
-i = 0
 print(split)
 
-for l in Lines:
+for i in range(37):
+    newtrain ='sortedimages/train/' + str(i+1)
+    newval = 'sortedimages/val/' + str(i+1)
+    if not os.path.exists(newtrain):
+        os.makedirs(newtrain)
+    if not os.path.exists(newval):
+        os.makedirs(newval)
+
+for idx,l in enumerate(Lines):
     s = l.split()
-    if s[2] == str(2):
-        species = 'dog/'
+    breed = str(s[1])+"/"
+    if idx%11 == 0:
+        os.rename(topfolder + s[0] + '.jpg', 'sortedimages/val/' + breed + s[0] + '.jpg')
     else:
-        species = 'cat/'
-    if i < 2700:
-        os.rename(topfolder + s[0] + '.jpg', 'sortedimages/train/' + species + s[0] + '.jpg')
-    else:
-        os.rename(topfolder + s[0] + '.jpg', 'sortedimages/val/' + species + s[0] + '.jpg')
-    i =i+1
+        os.rename(topfolder + s[0] + '.jpg', 'sortedimages/train/' + breed + s[0] + '.jpg')
